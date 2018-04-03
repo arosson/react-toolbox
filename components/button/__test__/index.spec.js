edbccf16523e543f5fc1400fe3ddd08102717c46
Andrea Bogazzi <abogazzi@shutterstock.com>
2018-04-03 22:30:27 +0200

    remove extra console.log

--- components/button/__test__/index.spec.js
+++ components/button/__test__/index.spec.js
@@ -6,7 +6,6 @@ import theme from '../theme.css';
 describe('Button', () => {
   describe('#render', () => {
     it('uses flat and neutral styles by default', () => {
-      console.log(theme);
       const wrapper = mount(<Button theme={theme} />);
       const { className } = wrapper.find('button').props();
       expect(className).toContain(theme.flat);
