e2de43386ba87461c33b5bf9cf76dc300bbb9582
Todd Beauchamp <tbeauchamp@premiumbeat.com>
2018-09-24 15:47:49 -0400

    FWA-XXX: Add check for Spacebar key code

--- components/dropdown/Dropdown.js
+++ components/dropdown/Dropdown.js
@@ -194,7 +194,11 @@ const factory = (Input) => {
           break;
         default:
           // If the current key pressed is a single character, add it to the typeahead accumulation string
-          if (singleCharWord.test(key) || key === ' ') { this.typeaheadAccumulator = this.typeaheadAccumulator + key; }
+          if (
+            singleCharWord.test(key)
+            || key === ' '
+            || key === 'Spacebar'
+          ) { this.typeaheadAccumulator = this.typeaheadAccumulator + key; }
           // Compare the typeahead string against the option values to find a match. The comparison is done in lower case so matching is not case sensitive
           const typeaheadMatchIndex = this.props.source.findIndex(({ label = '' }) => label.toLowerCase().startsWith(this.typeaheadAccumulator.toLowerCase()));
           // If a match is found, use its index as the focused option
